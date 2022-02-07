55484fcca7bcb70e3cf3685c00548c745273e8f1
TheoSks <97021089+TheoSks@users.noreply.github.com>
2022-02-07 19:23:20 +0100

    inscription + php + css + js

--- inscription.php
+++ inscription.php
@@ -1,30 +1,29 @@
-<?php include("partials/php/pdo.php");
+<?php include("pdo.php");
  
  $error = false;
 
  if (!empty($_POST)) {
     if (
-        empty($_POST["nom"]) || empty($_POST["prenom"])
-        || empty($_POST["email"]) || empty($_POST["password"])
+        empty($_POST["nom"]) || empty($_POST["email"]) 
+        || empty($_POST["password"])
     ) {
         $error = true;
     } else {
 
         $nom = $_POST["nom"];
-        $prenom = $_POST["prenom"];
         $email = $_POST["email"];
         $password = $_POST["password"];
         
 
-        $checkUser = $db->prepare("SELECT * FROM users WHERE email = '$email';");
+        $checkUser = $pdo->prepare("SELECT * FROM user WHERE email = '$email';");
         $checkUser->execute();
         $user = $checkUser->fetch();
 
         if (empty($user)) {
 
-            $RequestInsertUser = $db->prepare(
-                "INSERT INTO `users`(`nom`, `prenom`, `email`, `password`) 
-                    VALUES ('$nom','$prenom','$email' ,'$password')"
+            $RequestInsertUser = $pdo->prepare(
+                "INSERT INTO `user`(`nom`,`email`,`password`) 
+                    VALUES ('$nom','$email' ,'$password')"
             );
             if ($RequestInsertUser->execute()) {
                 header("location: index.php");
@@ -38,50 +37,54 @@
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=<bt, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="main.css">
     <title>Document</title>
 </head>
 <body>
-    <button class="main-btn"><a href="#">Get started</a></button> 
-    
-    <div class="modal" id="email-modal">
-        <div class="modal-content">
-            <span class="close-btn">&times;</span>
-            <div class="modal-content-left">
-                <img src="/images/image1.svg" alt="">
-            </div>
-            <div class="modal-content-left">
-                <form action="/" method="post" class="modal-form"
-                id="form">
-                <h1> Bienvenue sur le site ! inscrit-toi !</h1>
-                <div class="form-validation">
-                    <input type="text"class="modal-input" id="nom" name="nom" placeholder="Entrer votre nom">
-                        <p>Erreur Message<p>
-                </div>
-                <div class="form-validation">
-                    <input type="text"class="modal-input" id="prenom" name="prenom" placeholder="Entrer votre prenom">
-                        <p>Erreur Message<p>
-                </div>
-                <div class="form-validation">
-                    <input type="email"class="modal-input" id="email" name="email" placeholder="Entrer votre email">
-                        <p>Erreur Message<p>
-                </div>
-                <div class="form-validation">
-                    <input type="text"class="modal-input" id="password"  name="password" placeholder="Entrer votre mot de passe">
-                        <p>Erreur Message<p>
-                </div>
-                <input type="submit" class="modal-input-btn" value="Inscription">
-                <span class="modal-input-login"> Vous avez deja un compte ? inscrit-toi <a href="#">Inscription</a></span>
-            </form>
-            </div>
-        </div>
-    </div>
+<div class="container" id="container">
+	<div class="form-container sign-up-container">
+		<form action="#" method="post">
+			<h1>Créer un compte</h1>
+			<input type="text" name="nom" placeholder="nom" />
+			<input type="email" name="email" placeholder="email" />
+			<input type="password" name="password" placeholder="password" />
+			<button type="submit">Sign Up</button>
+		</form>
+	</div>
+	<div class="form-container sign-in-container">
+		<form action="#" method="post">
+			<h1>S'identifier</h1>
+			<input type="email" placeholder="email" />
+			<input type="password" placeholder="Password" />
+			<a href="#">Mot de passe oublié?</a>
+			<button type="submit">S'identifier</button>
+		</form>
+	</div>
+	<div class="overlay-container">
+		<div class="overlay">
+			<div class="overlay-panel overlay-left">
+				<h1>Bienvenue !</h1>
+				<p>Pour rester en contact avec nous, veuillez vous connecter avec vos informations personnelles</p>
+				<button class="ghost" id="signIn">S'identifier</button>
+			</div>
+			<div class="overlay-panel overlay-right">
+				<h1>Salut !</h1>
+				<p>Entrez vos données personnelles et commencez votre voyage avec nous</p>
+				<button class="ghost" id="signUp">S'inscrire</button>
+			</div>
+		</div>
+	</div>
+</div>
+<script src="inscription.js"></script>
 </body>
 </html>
+
+
+
+
--- pdo.php
+++ pdo.php
@@ -1,10 +1,5 @@
 <?php
 session_start();
-$pdo = new PDO('mysql:host=localhost;dbname=ath', "root", "root");
+$pdo = new PDO('mysql:host=localhost;dbname=sécurité', "root", "");
 
-define('DB_SERVER', 'ath');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('DB_NAME', 'vaccination');
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
