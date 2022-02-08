e8264fd2240db0937dba7c1c168a57b52dece0e0
ahmedooo1 <aahmadooo997@gmail.com>
2022-02-08 13:40:01 +0100

    update

--- inscription.js
+++ inscription.js
@@ -1,11 +1,28 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
+    const signUpButton = document.getElementById('signUp');
+    const signInButton = document.getElementById('signIn');
+    const container = document.getElementById('container');
 
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
 
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+
+    signUpButton.addEventListener('click', () => {
+        container.classList.add("right-panel-active");
+
+    });
+
+    signInButton.addEventListener('click', () => {
+        container.classList.remove("right-panel-active");
+    });
+
+
+    document.getElementById('button').addEventListener("click", function() {
+        document.querySelector('.Cardcontainer').style.display = "flex";
+
+
+    });
+
+    document.querySelector('.close').addEventListener("click", function() {
+        document.querySelector('.Cardcontainer').style.display = "none";
+
+    });
+
+    //loading
