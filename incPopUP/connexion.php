ce2977dd1014e2603aad78e45f9e1e82e86928a2
hlamelet <hlamelet@gmail.com>
2022-02-17 19:00:07 +0100

    hashage

--- incPopUP/connexion.php
+++ incPopUP/connexion.php
@@ -1,13 +1,21 @@
 <?php
 
+// print_r(password_hash("$password", PASSWORD_DEFAULT));
 if (isset($_POST['cemail'])) {
     $email = ($_POST['cemail']);
     $password = ($_POST['cpassword']);
 
-    $query = "SELECT * FROM user WHERE email='$email' and password='$password'";
+    // $query = "SELECT * FROM user WHERE email='$email' and password='$password'";
+    $query = "SELECT * FROM user WHERE email='$email'";
     $result = mysqli_query($connection, $query);
     $rows = mysqli_num_rows($result);
-    print_r($rows);
+
+    $requestemail = $pdo->prepare(
+        "SELECT * FROM user WHERE email='$email'"
+    );
+    $requestemail->execute();
+    $User = $requestemail->fetchAll();
+
 
     $requestnom = $pdo->prepare(
         "SELECT `nom` FROM user WHERE email='$email'"
@@ -15,8 +23,15 @@ if (isset($_POST['cemail'])) {
     $requestnom->execute();
     $nom_user = $requestnom->fetchAll();
 
+    $requestpassword = $pdo->prepare(
+        "SELECT `password` FROM user WHERE email='$email'"
+    );
+    $requestpassword->execute();
+    $hashedPassword = $requestpassword->fetchAll();
+    $hashedPassword = $hashedPassword[0][0];
+    print_r($hashedPassword);
 
-    if (mysqli_num_rows($result) == 1) {
+    if (password_verify($password, $hashedPassword) == 1) {
         $_SESSION["nom"] = $nom_user[0][0];
         $_SESSION["email"] = $email;
         header("location: index.php");
